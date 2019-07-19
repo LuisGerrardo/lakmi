3af52c5ab0c465b866e6fa32c9162ac7120d495b
YzakV <isaac.martinez@lakmisystems.com.mx>
2019-07-19 13:04:04 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
 let bNosotros = document.getElementById("nosotros");
-let bSecundaria = document.getElementById("barra-secundaria");
+let bSecundaria = document.getElementById("barra-sencundaria");
+
+
+bNosotros.onmouseover = () => {
+   bSecundaria.style.top = "50px";
+};
+bNosotros.onmouseleave = () =>{
+   bSecundaria.style.top = 0;
+};
+bSecundaria.onmouseover = () =>{
+   bSecundaria.style.top = "50px";
+};
+bSecundaria.onmouseleave = () =>{
+   bSecundaria.style.top = 0;
+};
