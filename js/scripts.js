fd805f51634d9ddc87332060e94d6ba34e0bd001
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-07-19 13:22:34 -0500

    3

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,28 @@ bSecundaria.onmouseover = () =>{
 bSecundaria.onmouseleave = () =>{
    bSecundaria.style.top = 0;
 };
+
+
+
+/*    SERVICIOS    */
+
+
+let nServicios = document.getElementById("servicios");
+let bSecundaria = document.getElementById("barra-sencundaria");
+
+
+bServicios.onmouseover = () => {
+   bSecundaria.style.top = "50px";
+};
+bServicios.onmouseleave = () =>{
+   bSecundaria.style.top = 0;
+};
+bSecundaria.onmouseover = () =>{
+   bSecundaria.style.top = "50px";
+};
+bSecundaria.onmouseleave = () =>{
+   bSecundaria.style.top = 0;
+};
+
+
+
